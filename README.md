bb187b11e21311c23e232046fd7fc6032e09cf3a
Danite <luansnain@gmail.com>
2025-06-22 11:02:17 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# CatChain
+
+A developer-focused tool for creating and managing auditable data pipelines for AI model training.
---
+++ src/catchain/cli.py
@@ -0,0 +1,13 @@
+import click
+
+
+@click.group()
+def main():
+    """
+    A developer-focused tool for creating and managing auditable data pipelines for AI model training.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
